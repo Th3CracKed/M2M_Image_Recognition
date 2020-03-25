6e948d974b7a6b55e1d80223854adcbef27a3375
Sylvestre Wampach <sylvestre.wampach@gmail.com>
2020-03-25 22:15:18 +0100

    remove useless Service

--- DataGenerationScript/main.py
+++ DataGenerationScript/main.py
@@ -1,11 +1,11 @@
+import TableService
 from DeviceServices import DeviceServices
 from DeviceSql import DeviceSql
 from EventSql import EventSql
-from SqlService import SqlService
 from TimeService import TimeService
 
 
-if __name__ == '__main__':
+def main():
     timeSrv = TimeService("2020-01-01 00:00:00", "2020-04-01 23:59:59", "%Y-%m-%d %H:%M:%S")
 
     my_events = ['homme', 'bouquetin', 'chamois', 'loup', 'loup', 'marmotte', 'Chouette', 'lièvre', 'lièvre']
@@ -14,9 +14,28 @@ if __name__ == '__main__':
     deviceSQL = DeviceSql(timeSrv, deviceSrv, 'device')
     eventSQL = EventSql(timeSrv, deviceSrv, 'event')
 
-    SqlService.generate_table_data(deviceSQL, 20)
-    SqlService.generate_table_data(eventSQL, 2000)
-
+    generate_table_data(deviceSQL, 20)
+    generate_table_data(eventSQL, 200)
 
     eventSQL.get_sql_table()
     deviceSQL.get_sql_table()
+
+
+def generate_table_data(table: TableService, nb_events: int):
+    # format outputs
+    output = ""
+
+    for i in range(0, nb_events):
+        output += table.get_values()
+        output += "\n"
+
+    print(output)
+
+    # Write inside a file
+    with open("results/res_events_" + table.get_tablename() + ".sql", "w") as f:
+        f.write(output)
+    print("Result saved in file: 'results/res_events_" + table.get_tablename() + ".sql'.")
+
+
+if __name__ == '__main__':
+    main()
